d33c77527f18e5a8e058b49588d745328c47f91d
Huyang <405586620@qq.com>
2021-04-03 20:17:55 +0800

    featconfig

--- example/index.ts
+++ example/index.ts
@@ -3,10 +3,10 @@ import { GameEngine } from '../src/index';
 let container = document.getElementById('snake');
 
 let engine = GameEngine.create(container, { 
-    width:800, // 画布宽度
-    height:800,  // 画布高度
-    speed:200,  // 蛇运动频率ms
-    snakeWidth:10, // 蛇的大小
+    width:600, // 画布宽度
+    height:600,  // 画布高度
+    speed:100,  // 蛇运动频率ms
+    snakeWidth:20, // 蛇的大小
 });
 
 console.log('engine',engine)
